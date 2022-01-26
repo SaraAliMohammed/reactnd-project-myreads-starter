ca5e7b3c32a6ac7280eb97a59770dcdca9bbf23b
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2022-01-26 11:10:50 +0200

    Make BookChanger Dynamic and show current shelf

--- src/BookShelfChanger.js
+++ src/BookShelfChanger.js
@@ -1,15 +1,39 @@
 import React, { Component } from "react";
 
+const shelfOptions = [
+    {
+        label: "Move to...",
+        value: "move",
+    },
+    {
+        label: "Currently Reading",
+        value: "currentlyReading",
+    },
+    {
+        label: "Banana",
+        value: "wantToRead",
+    },
+    {
+        label: "Want to Read",
+        value: "read",
+    },
+    {
+        label: "None",
+        value: "none",
+    }
+];
+
 class BookShelfChanger extends Component {
     render() {
+        const { shelf } = this.props;
+
         return (
             <div className="book-shelf-changer">
-                <select>
-                    <option value="move" disabled>Move to...</option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
+                <select defaultValue={shelf}>
+                    {shelfOptions.map((option) => (
+                        option.value==="move"?<option key={option.value} value={option.value} disabled>{option.label}</option>:
+                        <option key={option.value} value={option.value}>{option.label}</option>
+                    ))}
                 </select>
             </div>
         )
